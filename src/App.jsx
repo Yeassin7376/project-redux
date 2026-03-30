6b98b7e14a53c42b3dda8f2c4688a2006b1b9a56
Yeassin7376 <yeassinali17@gmail.com>
2026-03-30 17:19:38 +0600

    init: initila setup project

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+
+const App = () => {
+  return (
+    <div className="min-h-screen bg-gray-950 text-white">
+      <h1 className='text-3xl font-bold'>hello, world!</h1>
+    </div>
+  )
+}
+
+export default App
